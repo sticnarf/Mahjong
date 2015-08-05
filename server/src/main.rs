12f5a8c7b0c9e5878a7583576def2ea258ce80da
陈奕霖 <wormful@gmail.com>
2015-08-05 15:57:33 +0800

    server framework

---
+++ server/src/main.rs
@@ -0,0 +1,104 @@
+extern crate rand;
+
+use std::process;
+use std::process::*;
+use std::io::*;
+use std::thread;
+use std::env;
+use rand::*;
+use std::sync::*;
+use std::sync::mpsc::*;
+use std::collections::HashMap;
+
+fn main() {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 5 {
+        println!("{}", args[0]);
+        println!("Usage: ./server <AI1> <AI2> <AI3> <AI4>");
+        process::exit(1);
+    }
+    let mut board: HashMap<&str, i32> = HashMap::new();
+    for i in 0..4 {
+        board.insert(&args[i], 0);
+    }
+    let positions = vec![
+        [[1, 2, 3, 4], [2, 3, 4, 1], [3, 4, 1, 2], [4, 1, 2, 3]],
+        [[1, 3, 2, 4], [3, 2, 4, 1], [2, 4, 1, 3], [4, 1, 3, 2]],
+        [[1, 3, 4, 2], [3, 4, 2, 1], [4, 2, 1, 3], [2, 1, 3, 4]],
+        [[1, 4, 2, 3], [4, 2, 3, 1], [2, 3, 1, 4], [3, 1, 4, 2]],
+        [[1, 4, 3, 2], [4, 3, 2, 1], [3, 2, 1, 4], [2, 1, 4, 3]],
+        [[1, 2, 4, 3], [2, 4, 3, 1], [4, 3, 1, 2], [3, 1, 2, 4]],
+    ];
+    for group in &positions {
+        let seed = gen_seed();
+        for position in group {
+            let rng = StdRng::from_seed(&seed);
+            let paths = [
+                args[position[0]].clone(), args[position[1]].clone(), args[position[2]].clone(),
+                args[position[3]].clone()
+            ];
+            let game = Game::new(paths, rng);
+            game.start();
+        }
+    }
+}
+
+fn gen_seed() -> [usize; 8] {
+    let mut seed = [0; 8];
+    for i in 0..8 {
+        seed[i] = usize::rand(&mut rand::thread_rng());
+    }
+    seed
+}
+
+struct Game {
+    rng: StdRng,
+    paths: [String; 4]
+}
+
+impl Game {
+    fn new(paths: [String; 4], rng: StdRng) -> Game {
+        Game {
+            rng: rng,
+            paths: paths
+        }
+    }
+
+    fn start(&self) {
+        let mut inputs = Vec::new();
+        let (tx, rx) = mpsc::channel();
+        for i in 0..4 {
+            let command = Command::new(&self.paths[i])
+                              .stdin(Stdio::piped())
+                              .stdout(Stdio::piped())
+                              .spawn()
+                              .unwrap();
+            inputs.push(command.stdin.unwrap());
+            let tx = tx.clone();
+            let mut output = BufReader::new(command.stdout.unwrap());
+            thread::spawn(move || {
+                loop {
+                    let mut result = String::new();
+                    let size = output.read_line(&mut result).ok().unwrap();
+                    if size > 0 {
+                        tx.send(Message {
+                            id: i,
+                            message: result
+                        }).ok();
+                    }
+                    thread::sleep_ms(10);
+                }
+            });
+        }
+        loop {
+            let msg = rx.recv().ok().unwrap();
+            let id = msg.id;
+            let message = msg.message;
+        }
+    }
+}
+
+struct Message {
+    id: usize,
+    message: String
+}
